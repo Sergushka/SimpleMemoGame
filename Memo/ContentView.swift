644a04aa8384c96d0af5819785a3abea1c2b4812
Sender <cooladress@mail.ru>
2020-08-07 14:03:55 +0300

    Initial Commit

---
+++ Memo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by Alexander on 07.08.2020.
+//  Copyright © 2020 Link. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
