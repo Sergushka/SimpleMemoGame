c592851c00bb3993b1719e1096f8f11680ad9313
Sender <cooladress@mail.ru>
2020-08-07 14:15:13 +0300

    Add UI memo cards with ghost emoji

--- Memo/ContentView.swift
+++ Memo/ContentView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) {index in
+                MyView(isFaceUp: true)
+            }
+        }
+            .padding()
+            .font(Font.largeTitle)
+            .foregroundColor(Color.orange)
+    }
+}
+
+struct MyView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
